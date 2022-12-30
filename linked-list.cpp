c876c08e91ad68055d9172259ff56de6176885be
bad-at-names0 <mayank234pathak@gmail.com>
2022-12-30 22:21:20 +0530

    codeforces

--- linked-list.cpp
+++ linked-list.cpp
@@ -1,3 +1,111 @@
+// #include <bits/stdc++.h>
+// #define int long long int
+// #define fastio                        \
+//     ios_base::sync_with_stdio(false); \
+//     cin.tie(NULL);
+// #define pb push_back
+// #define mod 1000000007
+// using namespace std;
+
+// int32_t main()
+// {
+//     fastio int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int cnte = 0;
+//         int cnto = 0;
+//         int n;
+//         cin >> n;
+//         vector<int> v(n);
+
+//         for (int i = 0; i < n; i++)
+//         {
+//             cin >> v[i];
+//             if (v[i] % 2 == 0)
+//                 cnte++;
+//             else
+//                 cnto++;
+//         }
+
+//         map<int, int> mp;
+//         bool flag = false;
+//         for (auto &it : v)
+//         {
+//             if (mp[it] and it != 1)
+//             {
+//                 flag = true;
+//                 break;
+//             }
+//             else
+//             {
+//                 mp[it]++;
+//             }
+//         }
+
+//         if (flag)
+//         {
+//             cout << "NO\n";
+//             continue;
+//         }
+
+//         if (mp[1] < 2)
+//         {
+//             if (cnto > 1 and cnte > 1)
+//             {
+//                 cout << "NO\n";
+//             }
+//             else
+//             {
+//                 cout << "YES\n";
+//             }
+//             continue;
+//         }
+
+//         cnto -= mp[1];
+
+//         if (cnto > 1 and cnte > 1)
+//         {
+//             cout << "NO\n";
+//             continue;
+//         }
+
+//         cnto += mp[1];
+
+//         if (mp[1] > 1)
+//         {
+//             // if already satisfied this yes else no
+//             flag = false;
+//             for (int i = 0; i < n; i++)
+//             {
+//                 for (int j = i + 1; j < n; j++)
+//                 {
+//                     if (__gcd(v[i], v[j]) != 1)
+//                     {
+//                         flag = true;
+//                         break;
+//                     }
+//                 }
+//                 if (flag)
+//                     break;
+//             }
+//             if (flag)
+//             {
+//                 cout << "NO\n";
+//             }
+//             else
+//             {
+//                 cout << "YES\n";
+//             }
+//         }
+//         else
+//         {
+//             cout << "YES\n";
+//         }
+//     }
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 #define int long long int
 #define fastio                        \
@@ -7,24 +115,6 @@
 #define mod 1000000007
 using namespace std;
 
-void findlcs(string s, string p, int n, vector<vector<int>> &dp)
-{
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            if (s[i - 1] == p[j - 1])
-            {
-                dp[i][j] = 1 + dp[i - 1][j - 1];
-            }
-            else
-            {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
-}
-
 int32_t main()
 {
     fastio int t;
@@ -33,15 +123,96 @@ int32_t main()
     {
         int n;
         cin >> n;
-        string s;
-        cin >> s;
-        string copys = s;
-        reverse(s.begin(), s.end());
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        vector<int> v(n);
+        map<int, int> mp;
+        bool flag = false;
+        int cnte = 0;
+        int cnto = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+            if (v[i] % 2 == 0)
+            {
+                cnte++;
+            }
+            else
+            {
+                cnto++;
+            }
+        }
 
-        findlcs(s, copys, n, dp);
+        for (auto &it : v)
+        {
+            if (mp[it] and it != 1)
+            {
+                flag = true;
+                break;
+            }
+            else
+            {
+                mp[it]++;
+            }
+        }
 
-        cout << dp[n][n] / 2 << endl;
+        if (flag)
+        {
+            cout << "NO\n";
+            continue;
+        }
+        flag = false;
+
+        if (mp[1])
+        {
+            if (mp[1] > 1)
+            {
+                cnto -= mp[1];
+                cnto++;
+                flag = true;
+            }
+        }
+
+        if (cnto > 1 and cnte > 1)
+        {
+            cout << "NO\n";
+            continue;
+        }
+
+        flag = false;
+        if (mp[1])
+        {
+            if (mp[1] < 2)
+            {
+                cout << "YES\n";
+                continue;
+            }
+            else
+            {
+                flag = false;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (__gcd(v[i], v[j]) != 1)
+                        {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (flag)
+                        break;
+                }
+                if (flag)
+                    cout << "NO\n";
+                else
+                    cout << "YES\n";
+                continue;
+            }
+        }
+        else
+        {
+            cout << "YES\n";
+            continue;
+        }
     }
     return 0;
 }
