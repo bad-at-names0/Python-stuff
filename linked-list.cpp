c504582ba448e45613da3956444fc6462aaedb2f
bad-at-names0 <mayank234pathak@gmail.com>
2023-01-08 19:26:56 +0530

    Codeforcea

--- linked-list.cpp
+++ linked-list.cpp
@@ -9,19 +9,70 @@ using namespace std;
 
 int32_t main()
 {
-    fastio int n;
-    cin >> n;
-    vector<vector<int>> v(0, vector<int>(2));
-    for (int i = 0; i < n; i++)
+    fastio int t;
+    cin >> t;
+    while (t--)
     {
-        int x, y;
-        cin >> x >> y;
-        v.pb({x, y});
-    }
-    sort(v.begin(), v.end());
-    for (auto &it : v)
-    {
-        cout << it[0] << " " << it[1] << endl;
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+            mp[v[i]] = i;
+        }
+
+        int cnt = 0;
+        if (mp[1] != 1)
+        {
+            int posT = mp[1];
+            swap(v[1], v[mp[1]]);
+            mp[v[mp[1]]] = mp[1];
+            mp[1] = 1;
+            cnt++;
+        }
+        if (mp[2] != 0)
+        {
+            int posT = mp[2];
+            swap(v[0], v[mp[2]]);
+            mp[v[mp[2]]] = mp[2];
+            mp[2] = 0;
+            cnt++;
+        }
+
+        mp.clear();
+        for (int i = 2; i < n; i++)
+        {
+            mp[v[i]] = i;
+        }
+
+        // for (auto &it : mp)
+        // {
+        //     cout << it.first << " " << it.second << endl;
+        // }
+
+        // cout << endl;
+
+        for (int i = 2; i < n; i++)
+        {
+            if (v[i] != i + 1)
+            {
+                int whereIsVigoing = mp[i + 1];
+                swap(v[i], v[mp[i + 1]]);
+
+                mp[v[mp[i + 1]]] = mp[i + 1];
+                mp[i + 1] = i;
+
+                //     swap(v[0], v[mp[2]]);
+                // mp[v[mp[2]]] = mp[2];
+                // mp[2] = 0;
+                // // update mp of v[i] and i+1
+                cnt++;
+            }
+        }
+
+        cout << cnt << endl;
     }
     return 0;
 }
